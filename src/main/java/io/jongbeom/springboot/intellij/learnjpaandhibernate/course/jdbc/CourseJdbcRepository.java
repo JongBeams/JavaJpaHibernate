6de2ca94b2db2fc996d6a02567ae5eca146353b5
JongBeams <wwhow2003@gmail.com>
2025-10-31 23:15:10 +0900

    java spirng Jdbc 사용

---
+++ src/main/java/io/jongbeom/springboot/intellij/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,26 @@
+package io.jongbeom.springboot.intellij.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String INSERT_QUERY =
+            """
+                    insert into course (id,name,author)
+                    values (1,'LearnDB','JangJongBeom');
+            """;// 텍스트 블록
+
+    public void insert(){
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+
+
+
+
+}
